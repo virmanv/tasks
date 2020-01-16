aa40041b2cf2cccf72d627830a917ab2a94de931
Vidhi Virmani <Vidhi.Virmani@massive.co>
2020-01-16 13:20:57 +1100

    eks terraformation

---
+++ src/eks.tf
@@ -0,0 +1,50 @@
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.10"
+}
+
+locals {
+  cluster_name = "tf-eks-cluster"
+}
+
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = local.cluster_name
+  subnets      = aws_subnet.private.id
+  vpc_id       = aws_vpc.main.id
+
+  worker_groups_launch_template = [
+    {
+      name                 = "worker-group-1"
+      instance_type        = "t2.small"
+      asg_desired_capacity = 2
+      public_ip            = true
+    },
+    {
+      name                 = "worker-group-2"
+      instance_type        = "t2.medium"
+      asg_desired_capacity = 1
+      public_ip            = true
+    },
+  ]
+}
